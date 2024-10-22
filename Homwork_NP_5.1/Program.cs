1ab8f92cf4ab997e289d29ed8e7f0466cc9a4a8d
Hacha_tq55@student.itstep.org <Hacha_tq55@student.itstep.org>
2024-10-22 21:15:17 +0300

    Добавьте файлы проекта.

---
+++ Homwork_NP_5.1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Homwork_NP_5._1
+{
+    class Program
+    {
+        static HttpClient httpClient = new HttpClient();
+
+        static async Task Main()
+        {
+            // определяем данные запроса
+            using HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "https://www.google.com");
+
+            // получаем ответ
+            using HttpResponseMessage response = await httpClient.SendAsync(request);
+
+            // просматриваем данные ответа статус
+            Console.WriteLine($"Status: {response.StatusCode}\n");
+            //заголовки
+            Console.WriteLine("Headers");
+            foreach (var header in response.Headers)
+            {
+                Console.Write($"{header.Key}:");
+                foreach (var headerValue in header.Value)
+                {
+                    Console.WriteLine(headerValue);
+                }
+            }
+            
+        }
+    }
+
+}
